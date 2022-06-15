5e9feac59180bf001cc932c46059c48f513a5c53
Greytiger23 <isabelmothoa49@gmail.com>
2022-06-15 14:56:08 -0700

    first putchar

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - show the string
+ * @str: pointer vraiable
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+int s;
+s = *str;
+return (s);
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * _putchar - writes the character to stdout
+ * @c: character
+ * Return: 1 (Success)
+ */
+int _putchar(char c)
+{
+return (write(1, 8c, 1));
+}
