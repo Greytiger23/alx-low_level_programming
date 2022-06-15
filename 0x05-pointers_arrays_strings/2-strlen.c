ff709e80a0875a9f2e6f12243ac10d2747351d29
Greytiger23 <isabelmothoa49@gmail.com>
2022-06-15 11:33:12 -0700

    first string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - is a pointer variable to a character
+ * Return: Nothing
+ * @s: input character parameter
+ */
+int _strlen(char *s)
+{
+int a;
+a = 0;
+while (s[a] != '\0')
+{
+a++;
+}
+return(0);
+}
