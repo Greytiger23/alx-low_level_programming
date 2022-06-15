7e73bd4b2cf4f9cdf6198eb9f38562898ccc7cdf
Greytiger23 <isabelmothoa49@gmail.com>
2022-06-15 10:53:48 -0700

    first pointer

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * swap_int - pointer variable
+ * Return: Nothing
+ * @a: int parameter
+ * @b: int parameter
  */
-int main(void)
+void swap_int(int *a, int *b)
 {
-int a = 98;
-int b = 42;
-int *ip;
-int *ipb;
-ip = &b;
-ipb = &a;
-printf("a=%d, b=%d\n", *ip, *ipb);
-return(0);
+*a = 42;
+*b = 98;
 }
