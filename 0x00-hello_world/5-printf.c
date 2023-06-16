b9f6ec09ce115a88fe7b766d9a66931b6a1cfe9f
Greytiger23 <isabelmothoa49@gmail.com>
2023-06-15 17:19:02 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * this program is printing a message with proper grammer
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
