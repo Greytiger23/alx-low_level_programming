4639da34707f38a7665524c8c4514a126a2a25f4
Greytiger23 <isabelmothoa49@gmail.com>
2023-06-18 17:34:31 -0700

    print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * three digit combinations
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a, b, c;
+a = 0;
+while (a <= 9)
+{
+for (b = a + 1; b <= 9; b++)
+{
+for (c = b + 1; c <= 9; c++)
+{
+putchar(a + '0');
+putchar(b + '0');
+putchar(c + '0');
+if (a < 8 || b < 9 || c < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+a++;
+}
+putchar('\n');
+return (0);
+}
