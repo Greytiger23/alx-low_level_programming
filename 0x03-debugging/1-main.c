7edf7b4ef7f28f7bdffd29abf0de15d7838868a2
Greytiger23 <isabelmothoa49@gmail.com>
2023-06-22 10:27:53 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-        int i;
-//declarion of interger
-        printf("Infinite loop incoming :(\n");
-//message saying there is an infinite loop
-        i = 0;
-//declartion of interger i
-        while (i < 10)
-        {
-                putchar(i);
-        }
-//this while loop will end at 9
-        printf("Infinite loop avoided! \\o/\n");
-//it will print a message after while loop
-        return (0);
+int i;
+/* declarion of interger */
+printf("Infinite loop incoming :(\n");
+/* message saying there is an infinite loop */
+i = 0;
+/* declartion of interger i */
+while (i < 10)
+{
+putchar(i);
+}
+/* while doesnot have command that adds i */
+printf("Infinite loop avoided! \\o/\n");
+/* it will print a message after while loop */
+return (0);
 }
