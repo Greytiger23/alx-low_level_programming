4446e6c282d12c5f273e6c0f41e85c9631ab44e5
Greytiger23 <isabelmothoa49@gmail.com>
2023-06-22 10:44:56 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,11 @@ printf("Infinite loop incoming :(\n");
 /* message saying there is an infinite loop */
 i = 0;
 /* declartion of interger i */
-while (i < 10)
-{
-putchar(i);
-}
-/* while doesnot have command that adds i */
+/* while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ * while doesnot have command that adds i */
 printf("Infinite loop avoided! \\o/\n");
 /* it will print a message after while loop */
 return (0);
