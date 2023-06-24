d2811ca422f49be101c0e2c9fd826650b31afa28
Greytiger23 <isabelmothoa49@gmail.com>
2023-06-23 20:26:44 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,19 +10,19 @@ int main(void)
 int a;
 for (a = 1; a <= 100; a++)
 {
-if (a % 3 == 0)
+if ((a % 3) == 0)
 {
 printf("Fizz ");
 }
 else
 {
-if (a % 5 == 0)
+if ((a % 5) == 0)
 {
 printf("Buzz ");
 }
 else
 {
-if (a % 15 == 0)
+if ((a % 15) == 0)
 {
 printf("FizzBuzz ");
 }
