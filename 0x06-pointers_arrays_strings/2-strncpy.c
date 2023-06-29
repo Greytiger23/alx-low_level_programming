bac559285a909bff0975e3ec407e204864f9e92c
Greytiger23 <isabelmothoa49@gmail.com>
2023-06-29 14:22:11 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: char pointer variable
+ * @src: char pointer variable
+ * @n: first integer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src, n);
+return (dest);
+}
