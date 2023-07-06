63e06bf9a2050e562a2fa21df8a1b2594764b113
Greytiger23 <isabelmothoa49@gmail.com>
2023-07-05 19:30:30 -0700

    strlen recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _print_rev_recursion - function that prints reverse string
+ * @s: pointer variable
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+int a;
+a = *s;
+strrev(a);
+printf("%s\n", a);
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - function that returns length
+ * @s: pointer variable
+ * Return: void
+ */
+
+int _strlen_recursion(char *s)
+{
+int a;
+a = strlen(s);
+return (a);
+}
