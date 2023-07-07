a4a607ca1a5512a81dcc35f27353bff578c81f9c
Greytiger23 <isabelmothoa49@gmail.com>
2023-07-07 15:19:45 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that prints name
+ * @argc: first argument
+ * @argv: second argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", *argv);
+return (0);
+}
