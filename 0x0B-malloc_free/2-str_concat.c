22590cb5bda0d702634e3e9eb91abfc1baff16c3
Greytiger23 <isabelmothoa49@gmail.com>
2023-07-12 17:11:31 -0700

    str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,10 @@ char *str_concat(char *s1, char *s2)
 int a, n;
 char *b;
 b = malloc(strlen(s1) + strlen(s2) + 1);
+if (b == NULL)
+{
+return (NULL);
+}
 if (s1 == NULL)
 {
 s1 = " ";
@@ -21,10 +25,6 @@ s1 = " ";
 if (s2 == NULL)
 {
 s2 = " ";
-}
-if (b == NULL)
-{
-return (NULL);
 }
 for (a = 0; s1[a] != '\0'; a++)
 {
