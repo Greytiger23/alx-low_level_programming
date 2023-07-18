3226eeff13bb8a7356c1bc618daebf1ba8110e45
Greytiger23 <isabelmothoa49@gmail.com>
2023-07-17 20:35:43 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,24 +1,15 @@
-#ifndef dog_h
-#define dog_h
-#define dog_t
+#include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * struct dog - define new type
+ * new_dog - function that creates a new dog
  * @name: char
  * @age: float
  * @owner: char
+ * Return: void
  */
-struct dog
-{
-char *name;
-float age;
-char *owner;
-};
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-struct dog_t;
+
 struct dog_t *new_dog(char *name, float age, char *owner)
 {
 struct dog_t *b;
@@ -31,5 +22,4 @@ b->name = name;
 b->age = age;
 b->owner = owner;
 return b;
-};
-#endif
+}
