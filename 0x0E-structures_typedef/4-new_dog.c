c71e611a5305864946032c42859f7d2f87c3fe1d
Greytiger23 <isabelmothoa49@gmail.com>
2023-07-18 16:33:14 -0700

    free dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,13 +12,14 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-new_dog = malloc(sizeof(struct dog));
-if (new_dog === NULL)
+struct dog *d;
+d = malloc(sizeof(struct dog));
+if (d == NULL)
 {
 return (NULL);
 }
-new_dog->name = name;
-new_dog->age = age;
-new_dog->owner = owner;
-return new_dog;
+d->name = name;
+d->age = age;
+d->owner = owner;
+return (d);
 }
