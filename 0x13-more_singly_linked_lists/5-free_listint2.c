dbc74a924ac0f63824150865a6507fec5f7a0b82
Greytiger23 <isabelmothoa49@gmail.com>
2023-07-31 19:33:05 -0700

    free list int 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint2 - function that frees lis
+ * @head: pointer
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *a;
+listint_t *b = *head;
+while (b != NULL)
+{
+a = b->next;
+free(b);
+b = a;
+}
+*head = NULL;
+}
