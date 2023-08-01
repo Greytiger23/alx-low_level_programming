9e2cc99c055216725a7159c6cdf4024a0c26c5bb
Greytiger23 <isabelmothoa49@gmail.com>
2023-08-01 14:37:38 -0700

    get node int

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,12 +11,11 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-unsigned int x;
 if (head == NULL)
 {
 return (NULL);
 }
-for (x = 0; x < index; x++)
+while (index > 0)
 {
 head = head->next;
 index--;
