37b6ff23cbc8d34753ddef51a6dc62c6db8227b5
Greytiger23 <isabelmothoa49@gmail.com>
2023-10-02 18:26:10 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,9 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <stddef.h>
 #include <unistd.h>
-#include <sys/stat.h>
 #include <string.h>
 
 /**
@@ -31,7 +29,7 @@ if (text_content != NULL)
 {
 a = strlen(text_content);
 b = write(x, text_content, a);
-if (b == -1 || b != a)
+if (b == -1)
 {
 close(x);
 return (-1);
