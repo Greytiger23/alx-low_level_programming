ed4db5f08bf3c60e6501afdf0cefc6e8d1a753c0
Greytiger23 <isabelmothoa49@gmail.com>
2023-10-02 20:12:30 -0700

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,7 +21,7 @@ char buffer[BUFFER_SIZE];
 ssize_t x, y;
 if (ac != 3)
 {
-dprintf(2, "Usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 file_from = av[1];
@@ -33,19 +33,31 @@ while ((x = read(a, buffer, BUFFER_SIZE)) > 0)
 y = write(b, buffer, x);
 if (y == -1 || b == -1)
 {
-dprintf(2, "Error: Can't read from file %s\n", file_to);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_to);
 exit(99);
 }
 }
 if (x == -1 || a == -1)
 {
-dprintf(2, "Error: Can't read from file %s\n", file_from);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 exit(98);
 }
-if (close(a) == -1 || close(b) == -1)
+closefd(a);
+closefd(b);
+return (0);
+}
+/**
+ * closefd - function that closes the file descriptors
+ * @a: integer
+ * Return: void
+ */
+void closefd(int x)
 {
-dprintf(2, "Error: Can't close fd %d\n", a);
+int i;
+i = close(x);
+if (i == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", x);
 exit(100);
 }
-return (0);
 }
