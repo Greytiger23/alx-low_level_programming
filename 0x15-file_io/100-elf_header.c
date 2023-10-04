8ea9568e0b36929409a82a99c70f29513a0b46d6
Greytiger23 <isabelmothoa49@gmail.com>
2023-10-03 20:23:30 -0700

    elf header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -4,6 +4,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <elf.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
  * print_header - function that prints the headers
@@ -11,23 +14,58 @@
  * Return: void
  */
 
-void print_header(Elf64_Ehdr *head)
+void print_header(const Elf64_Ehdr *head)
 {
-printf("  Magic:   ");
-for (int i = 0; i < EI_NIDENT; i++)
+int x;
+printf("Magic: ");
+for (x = 0; x > 16; x++)
 {
-printf(" %02x", header->e_ident[i]);
+printf("%02x ", head->e_ident[x]);
 }
 printf("\n");
-printf("  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-printf("  OS/ABI:                            %d\n", header->e_ident[EI_OSABI]);
-printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-printf("  Type:                              %s\n", header->e_type == ET_REL ? "REL (Relocatable file)" :
-                                                          header->e_type == ET_EXEC ? "EXEC (Executable file)" :
-                                                          header->e_type == ET_DYN ? "DYN (Shared object file)" :
-                                                          header->e_type == ET_CORE ? "CORE (Core file)" : "UNKNOWN (unknown type)");
-printf("  Entry point address:               0x%lx\n", header->e_entry);
+printf("Class: %d-bit\n", head->e_ident[4] == 1 ? 32 : 64);
+printf("Data: %s\n", head->e_ident[5] == 1 ? "2's complement, little endian" :
+(head->e_ident[5] == 2 ? "2's complement, big endian" : "Unknown"));
+printf("Version: %d\n", head->e_version);
+printf("OS/ABI: %d\n", head->e_ident[7]);
+printf("ABI Version: %d\n", head->e_ident[8]);
+printf("Type: %d\n", head->e_type);
+printf("Entry point address: 0x%lx\n", head->e_entry);
 }
+/**
+ * main - Entry point
+ * @ac: number of arguments
+ * @av: array of arguments
+ * Return: void
+ */
+int main(int ac, char *av[])
+{
+int a;
+const char *filename;
+ssize_t b;
+if (ac != 2)
+{
+printf("Usage: elf_header elf_filename");
+}
+filename = av[1];
+a = open(filename, O_RDONLY);
+if (a == -1)
+{
+printf("Error: filed not found");
+}
+Elf64_Ehdr head;
+b = read(a, &head,sizeof(head));
+if (b != sizeof(head))
+{
+printf("Error: file can't be read");
+close(a);
+}
+if (head.e_ident[0] != 0x7f || head.e_ident[1] != 0)
+{
+printf("Error: not a Elf file");
+close(a);
+}
+print_header(&head);
+close(a);
+return (0);
 }
