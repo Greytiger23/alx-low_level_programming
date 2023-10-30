1e9b63a20432ade65ba32505b58d27f32736c4bc
Greytiger23 <isabelmothoa49@gmail.com>
2023-10-30 13:29:47 -0700

    file io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 
 #define BUFFER_SIZE 1024
+void mess(const char *m, const char *filename, int e);
 
 /**
  * main - program that copies the content of
@@ -17,26 +18,28 @@
 
 int main(int ac, char *av[])
 {
+const char *file_from, *file_to;
+int fd_from, fd_to;
+char buff[BUFFER_SIZE];
+ssize_t x, y;
 if (ac != 3)
 {
 mess("Usage: %s file_from file_to\n", av[0], 97);
 }
-const char *file_from = av[1];
-const char *file_to = av[2];
-int fd_from = open(file_from, O_RDONLY);
+file_from = av[1];
+file_to = av[2];
+fd_from = open(file_from, O_RDONLY);
 if (fd_from == -1)
 {
 mess("Error: Can't read from file %s\n", file_from, 98);
 }
-int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
+fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
 | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 if (fd_to == -1)
 {
 close(fd_from);
 mess("Error: Can't write to %s\n", file_to, 99);
 }
-char buff[BUFFER_SIZE];
-ssize_t x, y;
 while ((x = read(fd_from, buff, BUFFER_SIZE)) > 0)
 {
 y = write(fd_to, buff, x);
@@ -55,11 +58,13 @@ mess("Error: Can't read from file %s\n", file_from, 98);
 }
 if (close(fd_from) == -1)
 {
-mess("Error: Can't close fd %d\n", fd_from, 100);
+printf("Error: Can't close fd %d\n", fd_from);
+exit(100);
 }
 if (close(fd_to) == -1)
 {
-mess("Error: Can't close fd %d\n", fd_to, 100);
+printf("Error: Can't close fd %d\n", fd_to);
+exit(100);
 }
 return (0);
 }
