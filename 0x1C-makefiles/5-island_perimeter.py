3342639dd0084bb297b631b522b8a12f77c0ed0b
Greytiger23 <isabelmothoa49@gmail.com>
2024-02-21 17:08:51 -0800

    makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,17 +2,15 @@
 
 def island_perimeter(grid):
     a = 0
+    b = 0
     r = len(grid)
     c = len(grid[0])
     for x in range(r):
         for y in range(c):
             if grid[x][y] == 1:
-                if x == 0 or grid[x - 1][y] == 0:
-                    a += 1
-                if x == r - 1 or grid[x + 1][y] == 0:
-                    a += 1
-                if y == 0 or grid[x][y - 1] == 0:
-                    a += 1
-                if y == c - 1 or grid[x][y + 1] == 0:
-                    a += 1
-    return a
+                a += 1
+                if y > 0 and grid[x][y - 1] == 1:
+                    b += 1
+                if x > 0 and grid[x - 1][y] == 1:
+                    b += 1
+    return a * 4 - b * 2
